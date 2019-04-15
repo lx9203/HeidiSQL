755934c667ae87e22e4e183ac572c9155c7c9bff
lx9203 <lx9203@naver.com>
2019-04-15 17:50:32 +0900

    SQL

---
+++ 190402/insertSelect.sql
@@ -0,0 +1,1 @@
+rename table member to member_table;
---
+++ 190404/insertSelect.sql
@@ -0,0 +1,5 @@
+insert into bbs (id, memberId, title, date, content)
+select id, id, 'sdjfhjew', now(), 'fwehfwj'
+from member
+where id=100006;
+select * from bbs;
